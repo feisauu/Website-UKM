70b0d8e8e938caea2e997630d6e0e82ec218a9fc
053_MIB_Feis Aulia <70646439+feisauu@users.noreply.github.com>
2023-10-27 06:15:57 +0700

    Delete style directory

--- style/app.js
+++
@@ -1,9 +0,0 @@
-const faq = document.querySelectorAll (".faq .faqs");
-
-faq.forEach (faq => {
-    faq.addEventListener ("click", () => {
-    faq.classList.toggle("active");
-
-    
-     });
-});
