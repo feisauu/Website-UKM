2d458f98b20726d31be5abad2e67a6fe090311fc
tsamarahlubis <124503782+tsamarahlubis@users.noreply.github.com>
2023-10-26 22:33:41 +0700

    Add files via upload

---
+++ event/app.js
@@ -0,0 +1,29 @@
+const faq = document.querySelectorAll (".faq .faqs");
+
+faq.forEach (faq => {
+    faq.addEventListener ("click", () => {
+    faq.classList.toggle("active");
+
+    
+     });
+});
+
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehavior = "auto";
+});
+
+nextBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 900;
+});
+
+backBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 900;
+});
