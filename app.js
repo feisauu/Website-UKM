04cc1e1deaec42954a804584afa1c48db3e3baf1
tsamarahlubis <124503782+tsamarahlubis@users.noreply.github.com>
2023-11-05 20:59:30 +0700

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('.search').onclick = () =>{
+    searchForm.classList.toggle('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('.login').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('.menu').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+
+
+
 const faq = document.querySelectorAll (".faq .faqs");
 
 faq.forEach (faq => {
