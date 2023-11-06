8ca4f8fb4de0bcfb44f4c07e578618fc0162b777
tsamarahlubis <124503782+tsamarahlubis@users.noreply.github.com>
2023-11-06 16:56:46 +0700

    app.js

--- app.js
+++ app.js
@@ -30,6 +30,23 @@ window.onscroll = () =>{
 
 
 
+const content = document.querySelector('.Event .content');
+
+function checkScroll() {
+    const triggerPoint = content.getBoundingClientRect().top - window.innerHeight + 100;
+    if (triggerPoint < 0) {
+        content.classList.add('active');
+        window.removeEventListener('scroll', checkScroll);
+    }
+}
+
+window.addEventListener('scroll', checkScroll);
+checkScroll(); // Check on page load
+
+
+
+
+
 const faq = document.querySelectorAll (".faq .faqs");
 
 faq.forEach (faq => {
