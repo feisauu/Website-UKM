cb12d07617f89082f44167d79993d2a45d6f6e9b
tsamarahlubis <124503782+tsamarahlubis@users.noreply.github.com>
2023-11-06 18:52:45 +0700

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,56 @@
+//event judul
+const content = document.querySelector('.Event .content');
+
+function checkScroll() {
+    const triggerPoint = content.getBoundingClientRect().top - window.innerHeight + 100;
+    if (triggerPoint < 0) {
+        content.classList.add('active');
+        window.removeEventListener('scroll', checkScroll);
+    }
+}
+
+window.addEventListener('scroll', checkScroll);
+checkScroll(); // Check on page load
+
+
+//animasi judul
+// Fungsi untuk memeriksa apakah elemen berada dalam pandangan
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+
+}
+
+// Fungsi untuk menangani scrolling
+function handleScroll() {
+    const heading1 = document.querySelector('.about .heading');
+    const heading2 = document.querySelector('.retrospeksi .heading');
+
+    if (isElementInViewport(heading1)) {
+        heading1.style.opacity = '1';
+        heading1.style.transform = 'translateY(0)';
+    }
+
+    if (isElementInViewport(heading2)) {
+        heading2.style.opacity = '1';
+        heading2.style.transform = 'translateY(0)';
+    }
+}
+
+// Menambahkan event listener untuk event scroll
+window.addEventListener('scroll', handleScroll);
+
+// Panggil handleScroll saat halaman dimuat untuk memeriksa status awal
+handleScroll();
+
+
+
+//past memories
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('.search').onclick = () =>{
@@ -28,25 +81,7 @@ window.onscroll = () =>{
 }
 
 
-
-
-const content = document.querySelector('.Event .content');
-
-function checkScroll() {
-    const triggerPoint = content.getBoundingClientRect().top - window.innerHeight + 100;
-    if (triggerPoint < 0) {
-        content.classList.add('active');
-        window.removeEventListener('scroll', checkScroll);
-    }
-}
-
-window.addEventListener('scroll', checkScroll);
-checkScroll(); // Check on page load
-
-
-
-
-
+//faq
 const faq = document.querySelectorAll (".faq .faqs");
 
 faq.forEach (faq => {
